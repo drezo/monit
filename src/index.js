be9853472b4cc7f32a0a9eeb7730dc0183e74815
Drez0 <alexandr.fdh@gmail.com>
2018-07-24 12:03:56 +0300

    Add MongoDB & Mongoose module

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const debug = require('debug')('monit:index');
 
 const { promisify } = require('util');
-const { Server, Bot } = require('./bin');
+const { Server, Mongo, Bot } = require('./bin');
 
-const serverListen = port => promisify(Server.listen.bind(Server))(port);
+const serverListen = promisify(Server.listen.bind(Server));
+const mongoListen = promisify(Mongo.connect.bind(Mongo));
 
 const production = !(process.env.NODE_ENV === 'production');
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, MONGO_URI } = process.env;
 
 (async () => {
   try {
-    const [, bot] = await Promise.all([
+    const [, , bot] = await Promise.all([
+      mongoListen(MONGO_URI, { useNewUrlParser: true }),
       serverListen(SERVER_PORT),
       Bot.telegram.getMe()
     ]);
 
     const { address, port } = Server.address();
+
+    const {
+      options: { debug: MongoDebug }
+    } = Mongo;
 
     const { username, first_name: firstName } = bot;
 
@@ -24,9 +30,14 @@ const { SERVER_PORT } = process.env;
     console.log(`Server: ${address}:${port}, development: ${production}`);
 
     // eslint-disable-next-line no-console
+    console.log(`Database: ${process.env.MONGO_URI}, debugging: ${MongoDebug}`);
+
+    // eslint-disable-next-line no-console
     console.log(`Bot @${username} (name: ${firstName}) is running`);
 
     debug(`Server ${address}:${port}`);
+    debug(`Database ${MONGO_URI}, debug: ${MongoDebug}`);
+    debug(`Telegram Bot: username: @${username}, name: ${firstName})`);
   } catch (err) {
     // eslint-disable-next-line no-console
     console.log('Error happened during server start', err);
